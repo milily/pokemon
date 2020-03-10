a322ba41cc4f009afa566ad5853a1abd54fa795f
mrise <milae.rise@gmail.com>
2020-03-09 23:40:37 -0300

    ADD: subtitle by search

--- src/components/card.js
+++ src/components/card.js
@@ -10,22 +10,27 @@ const PokemonCard = props => {
 
     useEffect(() => {
         async function fetchPokemon(){
+            console.log("pokemon.pokemon", pokemon.pokemon)
             const newPokemonSprites = await getPokemon(pokemon.pokemon.name)
             setPokemonSprites(newPokemonSprites.front_default);
         }
-        fetchPokemon()
+
+        if(pokemon && pokemon.pokemon && pokemon.pokemon.name){
+            fetchPokemon()
+        }
     }, [])
 
     if(pokemon.data){
+        console.log("POKEMON", pokemon.data)
         return  (
-            <Col span={12}>
+            <Col span={6}>
                 <Card
                     hoverable
                     className="pokemon-card"
                     style={{ width: 240 }}
                     cover={<img alt="example" src={pokemon.data.sprites.front_default}/>}
                     >
-                    <Meta title={pokemon.data.name} description={pokemon.data.name} />
+                    <Meta title={pokemon.data.name} description={pokemon.data.abilities[0].ability.name} />
                 </Card>
             </Col>
         ) 
